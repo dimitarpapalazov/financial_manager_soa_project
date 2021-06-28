d41f5d256ddd568aa6b50fc49d0d237f2f94709a
dimitar-papalazov <dimitar.papalazov@students.finki.ukim.mk>
2021-06-29 00:28:59 +0200

    fix: transactions microservice fully functional

--- transactions_microservice/app.py
+++ transactions_microservice/app.py
@@ -1,13 +1,12 @@
-from settings_microservice.models import Category
 import connexion
 from flask import request, abort
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import jwt
-from ..user_microservice.models import User
-from datetime import datetime, timedelta
-from ..settings_microservice.app import db as settings_db
+from datetime import date, datetime, timedelta
 from calendar import monthrange
+import urllib
+import json
 
 
 # Authorization configuration
@@ -15,212 +14,174 @@ from calendar import monthrange
 def has_role(arg):
     def has_role_inner(fn):
         @wraps(fn)
-        def decorated_view(*args, **kwargs):
+        def decode_view(*args, **kwargs):
             try:
                 headers = request.headers
-                if 'AUTHORIZATION' in headers:
-                    token = headers['AUTHORIZATION'].split(' ')[1]
+                if 'Authorization' in headers:
+                    token = headers['Authorization']
                     decoded_token = decode_token(token)
-                    if 'admin' in decoded_token['roles']:
+                    if 'admin' in decoded_token:
                         return fn(*args, **kwargs)
                     for role in arg:
                         if role in decoded_token['roles']:
                             return fn(*args, **kwargs)
-                    abort(401)
+                    abort(404)
                 return fn(*args, **kwargs)
             except Exception as e:
                 abort(401)
-        return decorated_view
+
+        return decode_view
+
     return has_role_inner
 
 
 def decode_token(token):
+    token = token.strip('"')
     return jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
 
 
 # Endpoints
 
 def create(transaction_body):
-  found_user = db.session.query(User).get(transaction_body['user_id'])
-  if not found_user:
-    new_transaction = Transaction(date=datetime.now(),
-                          category_id=transaction_body['category_id'],
-                          account_id=transaction_body['account_id'],
-                          amount=transaction_body['amount'],
-                          description=transaction_body['description'],
-                          user_id=transaction_body['user_id'])
+    new_transaction = Transaction(date=date.today(),
+                                  category_id=transaction_body['category_id'],
+                                  account_id=transaction_body['account_id'],
+                                  amount=transaction_body['amount'],
+                                  description=transaction_body['description'],
+                                  user_id=transaction_body['user_id'])
     db.session.add(new_transaction)
     db.session.commit()
     return transaction_schema.dump(new_transaction)
-  else:
-    return 404
-  
 
-def get(transaction_id):
-  found_transaction = db.session.query(Transaction).get(transaction_id)
-  if found_transaction:
-    return transaction_schema.dump(found_transaction)
-  else:
-    return 404
+
+def get(tranasction_id):
+    found_transaction = db.session.query(Transaction).get(tranasction_id)
+    if found_transaction:
+        return transaction_schema.dump(found_transaction)
+    else:
+        return 404
+
+
+def transactions_by_category(get_body, transactions):
+    categories = urllib.request.urlopen(
+        'http://192.168.0.108:5003/api/settings/get-all-categories-by-user/' + str(get_body['user_id'])).read().decode('utf-8')
+    categories = json.loads(categories)
+    categories_ids = []
+    for i in range(len(categories)):
+      if categories[i]['type'] != get_body['type']:
+        categories.pop(i)
+        continue
+      if categories[i]['id'] not in categories_ids:
+        categories_ids.append(categories[i]['id'])
+    for i in range(len(transactions)):
+      if transactions[i].category_id not in categories_ids:
+        transactions.pop(i)
+    return transactions
 
 
 def get_daily(get_body):
-  found_user = db.session.query(User).get(get_body['user_id'])
-  if found_user:
     transactions = db.session.query(Transaction).filter(
-        user_id == get_body['user_id'], date == get_body['date']).all()
+        Transaction.user_id == get_body['user_id'], Transaction.date == get_body['date']).all()
     if get_body['type'] != -1:
-      categories = settings_db.session.query(Category).filter(type==get_body['type']).all()
-      categories = (category.id for category in categories)
-      for i in range(len(transactions)):
-        if transactions[i].category_id not in categories:
-          transactions.pop(i)
-    if get_body['to_JSON']:
-      return transaction_schema.dump(transactions, many=True)
-    else:
-      return transactions
-  else:
-    return 404
+        transactions = transactions_by_category(get_body, transactions)
+    return transaction_schema.dump(transactions, many=True)
 
 
 def get_weekly(get_body):
-  found_user=db.session.query(User).get(get_body['user_id'])
-  if found_user:
-    date = get_body['date']
-    start = date - timedelta(days=date.weekday())
+    date_body = get_body['date']
+    date_body = date.fromisoformat(date_body)
+    start = date_body - timedelta(days=date_body.weekday())
     end = start + timedelta(days=6)
     transactions = db.session.query(Transaction).filter(
-        user_id == get_body['user_id'], date >= start, date <= end).all()
+        Transaction.user_id == get_body['user_id'], Transaction.date >= start, Transaction.date <= end).all()
     if get_body['type'] != -1:
-      categories = settings_db.session.query(
-          Category).filter(type == get_body['type']).all()
-      categories = (category.id for category in categories)
-      for i in range(len(transactions)):
-        if transactions[i].category_id not in categories:
-          transactions.pop(i)
-    if get_body['to_JSON']:
-      return transaction_schema.dump(transactions, many=True)
-    else:
-      return transactions
-  else:
-    return 404
+      transactions = transactions_by_category(get_body, transactions)
+    return transaction_schema.dump(transactions, many=True)
 
 
 def get_monthly(get_body):
-  found_user = db.session.query(User).get(get_body['user_id'])
-  if found_user:
-    date = get_body['date']
-    start = date - timedelta(days=date.day - 1)
-    month, days = monthrange(date.year, date.month)
+    date_body = get_body['date']
+    date_body = date.fromisoformat(date_body)
+    start = date_body - timedelta(days=date_body.day - 1)
+    month, days = monthrange(date_body.year, date_body.month)
     end = start + timedelta(days=days - 1)
     transactions = db.session.query(Transaction).filter(
-        user_id == get_body['user_id'], date >= start, date <= end).all()
+        Transaction.user_id == get_body['user_id'], Transaction.date >= start, Transaction.date <= end).all()
     if get_body['type'] != -1:
-      categories = settings_db.session.query(
-          Category).filter(type == get_body['type']).all()
-      categories = (category.id for category in categories)
-      for i in range(len(transactions)):
-        if transactions[i].category_id not in categories:
-          transactions.pop(i)
-    if get_body['to_JSON']:
-      return transaction_schema.dump(transactions, many=True)
-    else:
-      return transactions
-  else:
-    return 404
+      transactions = transactions_by_category(get_body, transactions)
+    return transaction_schema.dump(transactions, many=True)
 
 
 def get_annually(get_body):
-  found_user = db.session.query(User).get(get_body['user_id'])
-  if found_user:
-    date = get_body['date']
-    day_of_year = date.timetuple().tm_yday
-    start = date - timedelta(days=day_of_year - 1)
-    month, days = monthrange(date.year, 2)
+    date_body = get_body['date']
+    date_body = date.fromisoformat(date_body)
+    day_of_year = date_body.timetuple().tm_yday
+    start = date_body - timedelta(days=day_of_year - 1)
+    month, days = monthrange(date_body.year, 2)
     end = start + timedelta(days=366) if days == 29 else start + timedelta(days=365)
     transactions = db.session.query(Transaction).filter(
-        user_id == get_body['user_id'], date >= start, date <= end).all()
+        Transaction.user_id == get_body['user_id'], Transaction.date >= start, Transaction.date <= end).all()
     if get_body['type'] != -1:
-      categories = settings_db.session.query(
-          Category).filter(type == get_body['type']).all()
-      categories = (category.id for category in categories)
-      for i in range(len(transactions)):
-        if transactions[i].category_id not in categories:
-          transactions.pop(i)
-    if get_body['to_JSON']:
-      return transaction_schema.dump(transactions, many=True)
-    else:
-      return transactions
-  else:
-    return 404
+      transactions = transactions_by_category(get_body, transactions)
+    return transaction_schema.dump(transactions, many=True)
 
 
 def get_period(period_body):
-  found_user = db.session.query(User).get(period_body['user_id'])
-  if found_user:
     transactions = db.session.query(Transaction).filter(
-        user_id == get_body['user_id'], date >= get_body['start'], date <= get_body['end']).all()
+        Transaction.user_id == period_body['user_id'], Transaction.date >= period_body['start'], Transaction.date <= period_body['end']).all()
     if period_body['type'] != -1:
-      categories = settings_db.session.query(
-          Category).filter(type == period_body['type']).all()
-      categories = (category.id for category in categories)
-      for i in range(len(transactions)):
-        if transactions[i].category_id not in categories:
-          transactions.pop(i)
-    if period_body['to_JSON']:
-      return transaction_schema.dump(transactions, many=True)
-    else:
-      return transactions
-  else:
-    return 404
+      transactions = transactions_by_category(period_body, transactions)
+    return transaction_schema.dump(transactions, many=True)
 
 
 def update_date(date_body):
-  found_transaction = db.session.query(
-      Transaction).get(date_body['transaction_id'])
-  if found_transaction:
-    found_transaction.date = date_body['date']
-    db.session.commit()
-    return transaction_schema.dump(found_transaction)
-  else:
-    return 404
+    found_transaction = db.session.query(
+        Transaction).get(date_body['tranasction_id'])
+    if found_transaction:
+        date_formatted = date.fromisoformat(date_body['date'])
+        found_transaction.date = date_formatted
+        db.session.commit()
+        return transaction_schema.dump(found_transaction)
+    else:
+        return 404
 
 
 def update_account(account_body):
-  found_transaction = db.session.query(
-      Transaction).get(account_body['transaction_id'])
-  if found_transaction:
-    found_transaction.account_id = account_body['account_id']
-    db.session.commit()
-    return transaction_schema.dump(found_transaction)
-  else:
-    return 404
+    found_transaction = db.session.query(
+        Transaction).get(account_body['tranasction_id'])
+    if found_transaction:
+        found_transaction.account_id = account_body['account_id']
+        db.session.commit()
+        return transaction_schema.dump(found_transaction)
+    else:
+        return 404
 
 
 def update_amount(amount_body):
-  found_transaction = db.session.query(
-      Transaction).get(amount_body['transaction_id'])
-  if found_transaction:
-    found_transaction.amount = amount_body['amount']
-    db.session.commit()
-    return transaction_schema.dump(found_transaction)
-  else:
-    return 404
+    found_transaction = db.session.query(
+        Transaction).get(amount_body['tranasction_id'])
+    if found_transaction:
+        found_transaction.amount = amount_body['amount']
+        db.session.commit()
+        return transaction_schema.dump(found_transaction)
+    else:
+        return 404
 
 
 def update_description(description_body):
-  found_transaction = db.session.query(
-      Transaction).get(description_body['transaction_id'])
-  if found_transaction:
-    found_transaction.description = description_body['date']
-    db.session.commit()
-    return transaction_schema.dump(found_transaction)
-  else:
-    return 404
-    
+    found_transaction = db.session.query(
+        Transaction).get(description_body['tranasction_id'])
+    if found_transaction:
+        found_transaction.description = description_body['description']
+        db.session.commit()
+        return transaction_schema.dump(found_transaction)
+    else:
+        return 404
 
-def delete_transaction(transaction_id):
-    db.session.query(Transaction).filter_by(id=transaction_id).delete()
+
+def delete_transaction(tranasction_id):
+    db.session.query(Transaction).filter_by(
+        id=tranasction_id).delete()
     db.session.commit()
 
 
@@ -240,4 +201,4 @@ transaction_schema = TransactionSchema()
 
 
 if __name__ == "__main__":
-    connexion_app.run(host='0.0.0.0', port=5000, debug=True)
+    connexion_app.run(host='0.0.0.0', port=5005, debug=True)
--- transactions_microservice/models.py
+++ transactions_microservice/models.py
@@ -1,22 +1,16 @@
 from app import db
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from ..user_microservice.models import User
-from ..settings_microservice.models import Category
-from ..accounts_microservice.models import Account
 
 
 class Transaction(db.Model):
     __tablename__ = 'transaction'
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
-    category = db.relationship('Category')
-    account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
-    account = db.relationship('Account')
+    date = db.Column(db.Date, nullable=False)
+    category_id = db.Column(db.Integer, nullable=False)
+    account_id = db.Column(db.Integer, nullable=False)
     amount = db.Column(db.Numeric, nullable=False)
     description = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship('User')
+    user_id = db.Column(db.Integer, nullable=False)
 
     def to_csv():
         return '{},{},{},{},{},{},{}'.format(self.id, self.date, self.category_id, self.account_id, self.amount, self.description, self.user_id)
