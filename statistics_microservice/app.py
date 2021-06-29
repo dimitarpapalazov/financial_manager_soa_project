32485f02a5d1701a6b16eed1aa445b013ebadba7
dimitar-papalazov <dimitar.papalazov@students.finki.ukim.mk>
2021-06-29 03:12:21 +0200

    fix: statistics microservice fully functional

--- statistics_microservice/app.py
+++ statistics_microservice/app.py
@@ -1,11 +1,14 @@
+from ast import parse
 import connexion
 from flask import request, abort
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import jwt
-from transactions_microservice.app import get_daily, get_weekly, get_monthly, get_annually, get_period
 import matplotlib.pyplot as plt
 import pathlib
+import json
+import urllib
+import requests
 
 
 # Authorization configuration
@@ -13,115 +16,140 @@ import pathlib
 def has_role(arg):
     def has_role_inner(fn):
         @wraps(fn)
-        def decorated_view(*args, **kwargs):
+        def decode_view(*args, **kwargs):
             try:
                 headers = request.headers
-                if 'AUTHORIZATION' in headers:
-                    token = headers['AUTHORIZATION'].split(' ')[1]
+                if 'Authorization' in headers:
+                    token = headers['Authorization']
                     decoded_token = decode_token(token)
-                    if 'admin' in decoded_token['roles']:
+                    if 'admin' in decoded_token:
                         return fn(*args, **kwargs)
                     for role in arg:
                         if role in decoded_token['roles']:
                             return fn(*args, **kwargs)
-                    abort(401)
+                    abort(404)
                 return fn(*args, **kwargs)
             except Exception as e:
                 abort(401)
-        return decorated_view
+
+        return decode_view
+
     return has_role_inner
 
 
 def decode_token(token):
+    token = token.strip('"')
     return jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
 
 
-def get_body(user_id, date, type, to_JSON):
+def get_body(user_id, date, type):
     return {'user_id': user_id,
             'date': date,
-            'type': type,
-            'to_JSON': to_JSON}
+            'type': type}
 
 
-def period_body(user_id, start, end, type, to_JSON):
+def period_body(user_id, start, end, type):
     return {'user_id': user_id,
             'start': start,
             'end': end,
-            'type': type,
-            'to_JSON': to_JSON}
+            'type': type}
 
 
 def sort_data_by_categories(data):
-    categories = (i.category_id for i in data)
+    categories = []
+    for i in data:
+        print(i['category_id'])
+        if i['category_id'] not in categories:
+            categories.append(i['category_id'])
+    print(len(categories))
     transactions_by_category = [0] * len(categories)
     for i in range(len(categories)):
         for t in data:
-            if t.category_id == categories[i]:
-                transactions_by_category[i] += t.amount
+            if t['category_id'] == categories[i]:
+                transactions_by_category[i] += t['amount']
     return categories, transactions_by_category
 
 
 def create_plot(data):
     categories, transactions = sort_data_by_categories(data)
     fig, ax = plt.subplots()
+    print(categories, transactions)
     ax.pie(transactions, labels=categories)
     ax.axis('equal')
     plt.savefig('pie_chart.png', dpi=300, bbox_inches='tight')
-    return {'pie_chart': pathlib.Path('pie_chart.png').absolute()}
+    return {'pie_chart': str(pathlib.Path('pie_chart.png').absolute())}
 
 # Endpoints
 
 
 def income_daily(body):
-    data = get_daily(get_body(body['user_id'], body['date'], 1, False))
-    return create_plot(data)
+    data = json.dumps(body)
+    r = requests.post(
+        'http://192.168.0.108:5005/api/transactions/get-daily', data=data, headers={'Content-Type': 'application/json'}).json()
+    return create_plot(r)
 
 
 def income_weekly(body):
-    data = get_weekly(get_body(body['user_id'], body['date'], 1, False))
-    return create_plot(data)
+    data = json.dumps(body)
+    r = requests.post(
+        'http://192.168.0.108:5005/api/transactions/get-weekly', data=data, headers={'Content-Type': 'application/json'}).json()
+    return create_plot(r)
 
 
 def income_monthly(body):
-    data = get_monthly(get_body(body['user_id'], body['date'], 1, False))
-    return create_plot(data)
+    data = json.dumps(body)
+    r = requests.post(
+        'http://192.168.0.108:5005/api/transactions/get-monthly', data=data, headers={'Content-Type': 'application/json'}).json()
+    return create_plot(r)
 
 
 def income_annually(body):
-    data = get_annually(get_body(body['user_id'], body['date'], 1, False))
-    return create_plot(data)
+    data = json.dumps(body)
+    r = requests.post(
+        'http://192.168.0.108:5005/api/transactions/get-annually', data=data, headers={'Content-Type': 'application/json'}).json()
+    return create_plot(r)
 
 
 def income_period(period_body):
-    data = get_annually(
-        get_body(period_body['user_id'], period_body['start'], period_body['end'], 1, False))
-    return create_plot(data)
+    data = json.dumps(period_body)
+    r = requests.post(
+        'http://192.168.0.108:5005/api/transactions/get-period', data=data, headers={'Content-Type': 'application/json'}).json()
+    return create_plot(r)
 
 
 def expense_daily(body):
-    data = get_daily(get_body(body['user_id'], body['date'], 0, False))
-    return create_plot(data)
+    data = json.dumps(body)
+    r = requests.post(
+        'http://192.168.0.108:5005/api/transactions/get-daily', data=data, headers={'Content-Type': 'application/json'}).json()
+    return create_plot(r)
 
 
 def expense_weekly(body):
-    data = get_weekly(get_body(body['user_id'], body['date'], 0, False))
-    return create_plot(data)
+    data = json.dumps(body)
+    r = requests.post(
+        'http://192.168.0.108:5005/api/transactions/get-weekly', data=data, headers={'Content-Type': 'application/json'}).json()
+    return create_plot(r)
 
 
 def expense_monthly(body):
-    data = get_monthly(get_body(body['user_id'], body['date'], 0, False))
-    return create_plot(data)
+    data = json.dumps(body)
+    r = requests.post(
+        'http://192.168.0.108:5005/api/transactions/get-monthly', data=data, headers={'Content-Type': 'application/json'}).json()
+    return create_plot(r)
 
 
 def expense_annually(body):
-    data = get_annually(get_body(body['user_id'], body['date'], 0, False))
-    return create_plot(data)
+    data = json.dumps(body)
+    r = requests.post(
+        'http://192.168.0.108:5005/api/transactions/get-annually', data=data, headers={'Content-Type': 'application/json'}).json()
+    return create_plot(r)
 
 
 def expense_period(period_body):
-    data = get_annually(
-        get_body(period_body['user_id'], period_body['start'], period_body['end'], 0, False))
-    return create_plot(data)
+    data = json.dumps(period_body)
+    r = requests.post(
+        'http://192.168.0.108:5005/api/transactions/get-period', data=data, headers={'Content-Type': 'application/json'}).json()
+    return create_plot(r)
 
 
 # Configuration
